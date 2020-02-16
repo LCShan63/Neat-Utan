12e0f8ba55cf33b6ff2b01f5158c82940d49d4ca
LCShan63 <60285888+LCShan63@users.noreply.github.com>
2020-02-17 03:00:01 +0800

    Addition - Steam Workshop Renovation
    Steam Workshop Renovation for TamperMonkey.

---
+++ TamperMonkey/Steam_Workshop_Renovation.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @author Deparsoul
+// @description Mitigation efforts to improve image previews in Steam UGC pages.
+// @downloadURL https://raw.githubusercontent.com/LCShan63/Neat-Utan/Archive/TamperMonkey/Steam_Workshop_Renovation.user.js
+// @grant None
+// @homepage https://userscripts.org/users/deparsoul/
+// @icon https://store.steampowered.com/favicon.ico
+// @include http*://steamcommunity.com/sharedfiles/filedetails/*
+// @include http*://steamcommunity.com/workshop/filedetails/*
+// @license GPL Version 3 or Any Later Version
+// @name Steam Workshop Renovation
+// @namespace https://lcshan63.github.io/
+// @run-at Document-End
+// @supportURL https://keylol.com/t95271-1-1
+// @updateURL https://github.com/LCShan63/Neat-Utan/raw/Archive/TamperMonkey/Steam_Workshop_Renovation.user.js
+// @version 1.00
+// ==/UserScript==
+
+if(typeof onYouTubeIframeAPIReady == 'function')
+    onYouTubeIframeAPIReady();
